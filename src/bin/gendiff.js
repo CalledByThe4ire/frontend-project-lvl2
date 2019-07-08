8e7f3d2c1c1d8f710dff590ddc109e054ce30693
Andrey Karamnoff <karamnoff@gmail.com>
2019-07-08 18:29:49 +0300

    add step1

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+
+import commander from 'commander';
+import genDiff from '..';
+
+const program = new commander.Command();
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f,  --format [type], Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((arg1, arg2) => genDiff(arg1, arg2))
+  .parse(process.argv);
+
+if (!program.args.length) program.help();
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export default (arg1, arg2) => {
+  console.log('Usage: gendiff [options] %s %s', arg1, arg2);
+};
