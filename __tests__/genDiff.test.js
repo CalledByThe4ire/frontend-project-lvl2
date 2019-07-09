d4ff3a0e5df48d05952cbedcb3e22c0537fe9530
Andrey Karamnoff <karamnoff@gmail.com>
2019-07-09 12:21:18 +0300

    add files for testing

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import genDiff from '../src';
+
+const fixuturesPath = path.join(__dirname, '__fixtures__');
+const before = JSON.stringify(
+  fs.readFileSync(path.join(fixuturesPath, 'before.json')),
+);
+const after = JSON.stringify(
+  fs.readFileSync(path.join(fixuturesPath, 'after.json')),
+);
+const result = fs.readFileSync(path.join(fixuturesPath, 'result.txt'), 'utf8');
+
+test('genDiff', () => {
+  expect(genDiff(before, after).replace(/\s/g, '')).toBe(result.replace(/\s/g, ''));
+});
