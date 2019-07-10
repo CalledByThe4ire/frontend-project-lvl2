89e3651be51de19492e2d5009e009eaefa83e706
Andrey Karamnoff <karamnoff@gmail.com>
2019-07-10 12:15:11 +0300

    rename 'helper' function

--- src/helpers.js
+++ src/helpers.js
@@ -3,4 +3,4 @@ eslint-disable import/prefer-default-export,
 implicit-arrow-linebreak,
 lodash/prefer-lodash-method
 */
-export const parse = str => str.trim().replace(/\s/g, '');
+export const removeWhitespaces = str => str.trim().replace(/\s/g, '');
