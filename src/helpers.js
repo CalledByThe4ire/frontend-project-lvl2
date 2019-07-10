70277f286027a7288aa56a6267c233c5fcb5257e
Andrey Karamnoff <karamnoff@gmail.com>
2019-07-10 14:32:42 +0300

    add 'convertDiffToArray' helper function

--- src/helpers.js
+++ src/helpers.js
@@ -4,3 +4,4 @@ implicit-arrow-linebreak,
 lodash/prefer-lodash-method
 */
 export const removeWhitespaces = str => str.trim().replace(/\s/g, '');
+export const convertDiffToArray = str => str.slice(str.indexOf('{') + 1, str.indexOf('}')).split('\n').filter(v => v);
