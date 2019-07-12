635cd40d493de6e77c3825bf69a7085e85d53baf
Andrey Karamnoff <karamnoff@gmail.com>
2019-07-12 08:17:33 +0300

    sort dependencies, add 'yaml' args to 'start' section for Makefile, extend tests for 'yaml' format

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,27 +1,33 @@
+/* eslint-disable lodash/prefer-lodash-method */
+
 import fs from 'fs';
 import path from 'path';
 import isEqual from 'lodash/isEqual';
 import sortBy from 'lodash/sortBy';
+
 import genDiff from '../src';
 import { convertDiffToArray } from '../src/helpers';
 
-const fixuturesPath = path.join(__dirname, '__fixtures__');
-const before = JSON.parse(
-  fs.readFileSync(path.join(fixuturesPath, 'before.json')),
-);
-const after = JSON.parse(
-  fs.readFileSync(path.join(fixuturesPath, 'after.json')),
-);
-const result = fs.readFileSync(path.join(fixuturesPath, 'result.txt'), 'utf8');
+const formats = {
+  json: 'json',
+  yaml: 'yml'
+};
 
-console.log(sortBy(convertDiffToArray(genDiff(before, after))));
-console.log(sortBy(convertDiffToArray(result)));
+Object.keys(formats).forEach(format => {
+  const fixuturesPath = path.join(__dirname, '__fixtures__');
+  const before = path.join(fixuturesPath, `${format}`, `before.${formats[format]}`);
+  const after = path.join(fixuturesPath, `${format}`, `after.${formats[format]}`);
+  const result = fs.readFileSync(
+    path.join(fixuturesPath, 'result.txt'),
+    'utf8'
+  );
 
-test('genDiff', () => {
-  expect(
-    isEqual(
-      sortBy(convertDiffToArray(genDiff(before, after))),
-      sortBy(convertDiffToArray(result)),
-    ),
-  ).toBeTruthy();
+  test(`genDiff ${format}`, () => {
+    expect(
+      isEqual(
+        sortBy(convertDiffToArray(genDiff(before, after))),
+        sortBy(convertDiffToArray(result))
+      )
+    ).toBeTruthy();
+  });
 });
