468d0e64caf8de037944dc54b775eb49ae4cedde
Andrey Karamnoff <karamnoff@gmail.com>
2019-07-26 08:09:17 +0300

    extend tests by adding formatters

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -10,23 +10,35 @@ const formats = {
   ini: 'ini',
 };
 
+const formatters = ['stringify', 'plain'];
+
 const fixuturesPath = path.join(__dirname, '__fixtures__');
-const result = fs.readFileSync(path.join(fixuturesPath, 'result.txt'), 'utf8');
-const table = Object.keys(formats).reduce((acc, format) => {
-  const before = path.join(
-    fixuturesPath,
-    `${format}`,
-    `before.${formats[format]}`,
-  );
-  const after = path.join(
-    fixuturesPath,
-    `${format}`,
-    `after.${formats[format]}`,
-  );
 
-  return [...acc, [before, after, result.trim()]];
-}, []);
+describe('gendiff', () => {
+  formatters.forEach((formatter) => {
+    describe(`output formatter: ${formatter}`, () => {
+      const result = fs.readFileSync(
+        path.join(fixuturesPath, `${formatter}.txt`),
+        'utf8',
+      );
+      const table = Object.keys(formats).reduce((acc, format) => {
+        const before = path.join(
+          fixuturesPath,
+          `${format}`,
+          `before.${formats[format]}`,
+        );
+        const after = path.join(
+          fixuturesPath,
+          `${format}`,
+          `after.${formats[format]}`,
+        );
 
-test.each(table)('genDiff %#', (before, after, expected) => {
-  expect(genDiff(before, after)).toBe(expected);
+        return [...acc, [format, before, after, result.trim()]];
+      }, []);
+
+      test.each(table)('%s', (format, before, after, expected) => {
+        expect(genDiff(before, after, formatter)).toBe(expected);
+      });
+    });
+  });
 });
