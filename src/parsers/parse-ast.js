da0c8805adc0d8fe11f3541b4f1177cb29e324da
Andrey Karamnoff <karamnoff@gmail.com>
2019-07-30 22:36:53 +0300

    fix naming-errors for parsers

--- src/parsers/parse-ast.js
+++ src/parsers/parse-ast.js
@@ -1,50 +1,50 @@
 /* eslint-disable implicit-arrow-linebreak */
 import { has } from 'lodash/fp';
 
-const hasChildren = (key, obj1, obj2) =>
-  obj1[key] instanceof Object && obj2[key] instanceof Object;
+const hasChildren = (key, object1, object2) =>
+  object1[key] instanceof Object && object2[key] instanceof Object;
 
-const getChildren = (key, obj1, obj2, func) => {
-  if (hasChildren(key, obj1, obj2)) {
-    return func(obj1[key], obj2[key]);
+const getChildren = (key, object1, object2, func) => {
+  if (hasChildren(key, object1, object2)) {
+    return func(object1[key], object2[key]);
   }
   return {};
 };
 
-const getTagForKeysDifference = (obj1, obj2) =>
-  Object.keys(obj2).filter(key => !Object.keys(obj1).includes(key));
+const getTagForKeysDifference = (object1, object2) =>
+  Object.keys(object2).filter(key => !Object.keys(object1).includes(key));
 
-const getType = (key, obj1, obj2) => {
-  if (has(key, obj1) && !has(key, obj2)) {
+const getType = (key, object1, object2) => {
+  if (has(key, object1) && !has(key, object2)) {
     return 'removed';
   }
-  if (has(key, obj1) && has(key, obj2)) {
-    if (hasChildren(key, obj1, obj2) || obj1[key] === obj2[key]) {
+  if (has(key, object1) && has(key, object2)) {
+    if (hasChildren(key, object1, object2) || object1[key] === object2[key]) {
       return 'unchanged';
     }
-    if (obj1[key] !== obj2[key]) {
+    if (object1[key] !== object2[key]) {
       return 'changed';
     }
   }
   return undefined;
 };
 
-const createByType = (type, key, obj1, obj2, func) => {
-  const children = getChildren(key, obj1, obj2, func);
+const createByType = (type, key, object1, object2, func) => {
+  const children = getChildren(key, object1, object2, func);
   switch (type) {
     case 'changed':
       return {
         key,
         value: {
-          before: obj1[key],
-          after: obj2[key],
+          before: object1[key],
+          after: object2[key],
         },
       };
     case 'unchanged':
     case 'removed':
       return {
         key,
-        ...(Object.keys(children).length === 0 && { value: obj1[key] }),
+        ...(Object.keys(children).length === 0 && { value: object1[key] }),
         ...(Object.keys(children).length !== 0 && { children }),
       };
     default:
@@ -53,8 +53,8 @@ const createByType = (type, key, obj1, obj2, func) => {
 };
 
 const buildAST = (data1, data2) => {
-  const obj1 = data1;
-  const obj2 = data2;
+  const object1 = data1;
+  const object2 = data2;
 
   const root = {
     added: [],
@@ -63,14 +63,14 @@ const buildAST = (data1, data2) => {
     removed: [],
   };
 
-  return Object.keys(obj1).reduce((acc, key) => {
-    const type = getType(key, obj1, obj2);
-    const objByType = createByType(type, key, obj1, obj2, buildAST);
-    const newKeys = getTagForKeysDifference(obj1, obj2);
-    const newKeyValuePairs = newKeys.map(k => ({ key: k, value: obj2[k] }));
+  return Object.keys(object1).reduce((acc, key) => {
+    const type = getType(key, object1, object2);
+    const objectByType = createByType(type, key, object1, object2, buildAST);
+    const newKeys = getTagForKeysDifference(object1, object2);
+    const newKeyValuePairs = newKeys.map(k => ({ key: k, value: object2[k] }));
     const newAcc = { ...acc };
     newAcc.added = newAcc.added.length !== 0 ? newAcc.added : newKeyValuePairs;
-    newAcc[type].push(objByType);
+    newAcc[type].push(objectByType);
     return {
       ...acc,
       ...newAcc,
