70916c7941c3182acd58b38a8ec5862a34b49d47
Andrey Karamnoff <karamnoff@gmail.com>
2019-08-06 15:33:43 +0300

    add 'json' formatter

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,65 +1,21 @@
-import { identity } from 'lodash/fp';
+const countNumberOfStateChange = (ast, state) =>
+  ast.reduce((acc, data) => {
+    const { type, children } = data;
+    const newAcc = type !== state ? acc : acc + 1;
 
-const propertyActions = [
-  {
-    type: 'added',
-    checkType() {
-      return this.type;
-    },
-    updateNumberOfPropertyStateChange: (previousValue, tree, key) => previousValue + tree[key].length,
-  },
-  {
-    type: 'removed',
-    checkType() {
-      return this.type;
-    },
-    updateNumberOfPropertyStateChange: (previousValue, tree, key) => previousValue + tree[key].length,
-  },
-  {
-    type: 'changed',
-    checkType() {
-      return this.type;
-    },
-    updateNumberOfPropertyStateChange: (previousValue, tree, key) => previousValue + tree[key].length,
-  },
-  {
-    type: 'unchanged',
-    checkType() {
-      return this.type;
-    },
-    updateNumberOfPropertyStateChange: identity,
-  },
-];
-
-const getPropertyAction = arg =>
-  propertyActions.find(value => arg === value.checkType());
-
-const countNumberOfPropertyStateChange = (ast, state) =>
-  Object.keys(ast).reduce((acc, keyType) => {
-    if (keyType !== state) {
-      return acc;
+    if (!children) {
+      return newAcc;
     }
-
-    const { updateNumberOfPropertyStateChange } = getPropertyAction(keyType);
-    const newAcc = updateNumberOfPropertyStateChange(acc, ast, keyType);
-
-    return ast.unchanged.reduce((iAcc, value) => {
-      const { children } = value;
-      const newIAcc = keyType === 'unchanged' ? iAcc + 1 : iAcc;
-      if (!children) {
-        return newIAcc;
-      }
-      return newIAcc + countNumberOfPropertyStateChange(children, state);
-    }, newAcc);
+    return newAcc + countNumberOfStateChange(children, state);
   }, 0);
 
 export default ast =>
   JSON.stringify(
     {
-      addedPropertiesCount: countNumberOfPropertyStateChange(ast, 'added'),
-      removedPropertiesCount: countNumberOfPropertyStateChange(ast, 'removed'),
-      changedPropertiesCount: countNumberOfPropertyStateChange(ast, 'changed'),
-      unchangedPropertiesCount: countNumberOfPropertyStateChange(ast, 'unchanged'),
+      addedPropertiesCount: countNumberOfStateChange(ast, 'added'),
+      removedPropertiesCount: countNumberOfStateChange(ast, 'removed'),
+      changedPropertiesCount: countNumberOfStateChange(ast, 'changed'),
+      unchangedPropertiesCount: countNumberOfStateChange(ast, 'unchanged'),
     },
     null,
     '\t',
