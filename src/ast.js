3dc52fa3b2555b88591424b170cd2b51ece1dba2
Andrey Karamnoff <karamnoff@gmail.com>
2019-08-08 16:14:12 +0300

    make ast' refactoring

--- src/ast.js
+++ src/ast.js
@@ -1,34 +1,47 @@
-import { has } from 'lodash/fp';
+import { has, union } from 'lodash/fp';
 
 const buildAST = (data1 = {}, data2 = {}) => {
   const object1 = data1;
   const object2 = data2;
 
-  const object2Keys = Object.keys(object2)
-    .filter(key => !Object.keys(object1).includes(key))
-    .map(newKey => ({
-      type: 'added',
-      name: newKey,
-      value: object2[newKey],
-    }));
-
-  const object1Keys = Object.keys(object1).reduce((acc, key) => {
-    if (has(key, object2)) {
-      if (object1[key] instanceof Object && object2[key] instanceof Object) {
+  return union(Object.keys(object1), Object.keys(object2)).reduce(
+    (acc, key) => {
+      if (has(key, object1) && has(key, object2)) {
+        if (object1[key] instanceof Object && object2[key] instanceof Object) {
+          return [
+            ...acc,
+            {
+              type: 'unchanged',
+              name: key,
+              children: buildAST(object1[key], object2[key]),
+            },
+          ];
+        }
+        if (object1[key] === object2[key]) {
+          return [
+            ...acc,
+            {
+              type: 'unchanged',
+              name: key,
+              value: object1[key],
+            },
+          ];
+        }
         return [
           ...acc,
           {
-            type: 'unchanged',
+            type: 'changed',
             name: key,
-            children: buildAST(object1[key], object2[key]),
+            valueBefore: object1[key],
+            valueAfter: object2[key],
           },
         ];
       }
-      if (object1[key] === object2[key]) {
+      if (has(key, object1) && !has(key, object2)) {
         return [
           ...acc,
           {
-            type: 'unchanged',
+            type: 'removed',
             name: key,
             value: object1[key],
           },
@@ -37,23 +50,14 @@ const buildAST = (data1 = {}, data2 = {}) => {
       return [
         ...acc,
         {
-          type: 'changed',
+          type: 'added',
           name: key,
-          valueBefore: object1[key],
-          valueAfter: object2[key],
+          value: object2[key],
         },
       ];
-    }
-    return [
-      ...acc,
-      {
-        type: 'removed',
-        name: key,
-        value: object1[key],
-      },
-    ];
-  }, []);
-  return [...object1Keys, ...object2Keys];
+    },
+    [],
+  );
 };
 
 export default buildAST;
