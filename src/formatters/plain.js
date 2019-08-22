555def8609f14b1d7649243684de1a4d171eb2fe
Andrey Karamnoff <karamnoff@gmail.com>
2019-08-22 10:42:20 +0300

    make refactoring for 'plain' formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,3 +1,5 @@
+import { flattenDeep } from 'lodash/fp';
+
 const mapValue = (value = '') => {
   if (value instanceof Object) {
     return '[complex value]';
@@ -11,20 +13,20 @@ const mapValue = (value = '') => {
 const typeActions = {
   inner: (name, args, fn) => {
     const { children } = args;
-    return children.reduce((acc, child) => `${acc}${fn(child)}`, '');
+    return fn(children);
   },
   added: (name, args) => {
     const { value } = args;
     const mappedValue = mapValue(value);
-    return `Property '${name}' was added with value: ${mappedValue}\n`;
+    return `Property '${name}' was added with value: ${mappedValue}`;
   },
-  removed: name => `Property '${name}' was removed\n`,
+  removed: name => `Property '${name}' was removed`,
   changed: (name, args) => {
     const { valueBefore, valueAfter } = args;
     const mappedValueBefore = mapValue(valueBefore);
     const mappedValueAfter = mapValue(valueAfter);
 
-    return `Property '${name}' was updated. From ${mappedValueBefore} to ${mappedValueAfter}\n`;
+    return `Property '${name}' was updated. From ${mappedValueBefore} to ${mappedValueAfter}`;
   },
   unchanged: () => '',
 };
@@ -40,36 +42,37 @@ const hasSearchName = (data = {}, searchName = '') => {
   return false;
 };
 
-const buildComplexName = (ast = [], searchName = '') => ast.reduce((acc, data) => {
-  if (!hasSearchName(data, searchName)) {
+const buildComplexName = (ast = [], searchName = '') =>
+  ast.reduce((acc, data) => {
+    if (!hasSearchName(data, searchName)) {
+      return acc;
+    }
+    const { name, children } = data;
+
+    if (children) {
+      return [...acc, name, ...buildComplexName(children, searchName)];
+    }
+
+    if (name === searchName) {
+      return [...acc, name];
+    }
     return acc;
-  }
-  const { name, children } = data;
-
-  if (children) {
-    return [...acc, name, ...buildComplexName(children, searchName)];
-  }
-
-  if (name === searchName) {
-    return [...acc, name];
-  }
-  return acc;
-}, []);
-
-const sort = (str = '') =>
-  str
-    .split('\n')
-    .sort()
-    .join('\n');
+  }, []);
 
 export default (ast = []) => {
-  const mapData = (data) => {
-    const { type, name, ...rest } = data;
-    const buildAcc = typeActions[type];
-    const complexName = buildComplexName(ast, name).join('.');
-    const acc = buildAcc(complexName, rest, mapData);
-    return acc;
-  };
-  const result = ast.reduce((acc, entry) => `${acc}${mapData(entry)}`, '');
-  return sort(result);
+  const reduce = data =>
+    data.reduce((acc, entry) => {
+      const { type, name, ...rest } = entry;
+      const buildAcc = typeActions[type];
+      const complexName = buildComplexName(ast, name).join('.');
+      const newAcc = [...acc, buildAcc(complexName, rest, reduce)];
+      return newAcc;
+    }, []);
+
+  const reduced = reduce(ast);
+  const flattened = flattenDeep(reduced);
+  const filtered = flattened.filter(string => string);
+  const stringified = filtered.join('\n');
+
+  return stringified;
 };
