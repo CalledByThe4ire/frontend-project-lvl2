d524eddf25d83175efcd1ea9010f8f6d41b0aa95
Andrey Karamnoff <karamnoff@gmail.com>
2019-08-30 20:55:04 +0300

    rewrite 'diff' formatter, update 'diff' result test

--- src/formatters/diff.js
+++ src/formatters/diff.js
@@ -1,70 +1,50 @@
-const stringify = (data, times, step) => {
-  if (data instanceof Object) {
-    const result = Object.keys(data).reduce(
-      (acc, key) => [...acc, `${' '.repeat(times + step)}${key}: ${data[key]}`],
-      [],
-    );
-    return `{\n${result.join('\n')}\n${' '.repeat(times)}}`;
+import { flatten } from 'lodash/fp';
+
+const calcOffset = (factor, times = 2) => ' '.repeat(times * factor);
+
+const stringifyValue = (value, factor) => {
+  if (!(value instanceof Object)) {
+    return value;
   }
-  return data;
+  return `{\n${[...Object.keys(value)].map(
+    key => `${calcOffset(factor + 1)}${key}: ${value[key]}`,
+  )}\n${calcOffset(factor)}}`;
 };
 
 const typeActions = {
-  inner: (props, times, step, fn) => {
+  inner: (props, offset, factor, fn) => {
     const { name, children } = props;
-    return `${' '.repeat(times + step)}${name}: {\n${fn(
-      children,
-      times + step * 2,
-      step,
-    ).join('\n')}\n${' '.repeat(times + step)}}`;
+    return `${offset}${name}: {\n${flatten(fn(children, factor + 1)).join(
+      '\n',
+    )}\n${offset}}`;
   },
-  added: (props, times, step) => {
+  added: (props, offset, factor) => {
     const { name, value } = props;
-    return `${' '.repeat(times)}+ ${name}: ${stringify(
-      value,
-      times + step,
-      step,
-    )}`;
+    return `${offset}+ ${name}: ${stringifyValue(value, factor)}`;
   },
-  removed: (props, times, step) => {
+  removed: (props, offset, factor) => {
     const { name, value } = props;
-    return `${' '.repeat(times)}- ${name}: ${stringify(
-      value,
-      times + step,
-      step,
-    )}`;
+    return `${offset}+ ${name}: ${stringifyValue(value, factor)}`;
   },
-  changed: (props, times, step) => {
+  changed: (props, offset, factor) => {
     const { name, valueBefore, valueAfter } = props;
     return [
-      `${' '.repeat(times)}- ${name}: ${stringify(
-        valueBefore,
-        times + step,
-        step,
-      )}`,
-      `${' '.repeat(times)}+ ${name}: ${stringify(
-        valueAfter,
-        times + step,
-        step,
-      )}`,
-    ].join('\n');
+      `${offset}- ${name}: ${stringifyValue(valueBefore, factor)}`,
+      `${offset}+ ${name}: ${stringifyValue(valueAfter, factor)}`,
+    ];
   },
-  unchanged: (props, times, step) => {
+  unchanged: (props, offset, factor) => {
     const { name, value } = props;
-    return `${' '.repeat(times + step)}${name}: ${stringify(
-      value,
-      times + step,
-      step,
-    )}`;
+    return `${offset}${name}: ${stringifyValue(value, factor)}`;
   },
 };
-
 export default (ast) => {
-  const map = (data, times, step) => data.map((entry) => {
+  const iter = (data, factor = 1) => data.map((entry) => {
     const { type, ...props } = entry;
-    const buildAcc = typeActions[type];
-    return [buildAcc(props, times, step, map)];
+    const offset = calcOffset(factor);
+    const action = typeActions[type];
+
+    return action(props, offset, factor, iter);
   });
-  const result = map(ast, 2, 2);
-  return `{\n${result.join('\n')}\n}`;
+  return `{\n${flatten(iter(ast)).join('\n')}\n}`;
 };
